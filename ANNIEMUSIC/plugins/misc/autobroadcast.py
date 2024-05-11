1c89a8b9903687b05e421c1d904ea39671746049
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-05-11 20:38:01 +0530

    Update autobroadcast.py

--- ANNIEMUSIC/plugins/misc/autobroadcast.py
+++ ANNIEMUSIC/plugins/misc/autobroadcast.py
@@ -6,7 +6,6 @@ from config import START_IMG_URL
 from ANNIEMUSIC.utils.database import get_served_chats
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-START_IMG_URLS = "https://telegra.ph/file/f6aca1c3d811f3164afb2.jpg"
 
 MESSAGE = f"""**╭───────────────────⦿
   │☄️ ▸ ɪ ʜᴀᴠᴇ sᴘᴇᴄɪᴀʟ ғᴇᴀᴛᴜʀᴇs
@@ -40,6 +39,7 @@ async def send_message_to_chats():
             chat_id = chat_info.get('chat_id')
             if isinstance(chat_id, int):  # Check if chat_id is an integer
                 try:
+                    await app.send_photo(chat_id, photo=START_IMG_URL, caption=MESSAGE, reply_markup=BUTTON)
                     await asyncio.sleep(3)  # Sleep for 1 second between sending messages
                 except Exception as e:
                     pass  # Do nothing if an error occurs while sending message
