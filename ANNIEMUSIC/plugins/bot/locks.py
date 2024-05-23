384526904422a19a72381977b9edfbf651881bd5
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-05-23 09:30:49 +0530

    Update locks.py

--- ANNIEMUSIC/plugins/bot/locks.py
+++ ANNIEMUSIC/plugins/bot/locks.py
@@ -2,7 +2,7 @@ import html
 from typing import Optional, List
 
 import telegram.ext as tg
-from telegram import Message, Chat, Update, Bot, , User, MessageEntity
+from telegram import Message, Chat, Update, Bot, User, MessageEntity
 from telegram import TelegramError
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, Filters
